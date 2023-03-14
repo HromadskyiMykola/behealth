f36a2979202b52b357e48aa8b3cf777fb910e369
Aptechka <samsungt285v@gmail.com>
2023-03-14 23:56:45 +0200

    Add ThemeProvider in index.tsx, create theme.provider folder and create components folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
-
+import {CssBaseline, ThemeProvider} from '@mui/material';
 import App from './App';
+import {theme} from "./theme.provider";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  // <React.StrictMode>
-  <>
+
+  <ThemeProvider theme={theme} >
     <CssBaseline />
     <App />
-  </>
-  // </React.StrictMode>
+  </ThemeProvider>
+
 );
