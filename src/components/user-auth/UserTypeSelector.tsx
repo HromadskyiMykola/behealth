282b409cde84abfd6acf3271b3e9d776c5087ee5
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-04-03 12:59:28 +0300

    move types to dev-deps

--- src/components/user-auth/UserTypeSelector.tsx
+++ src/components/user-auth/UserTypeSelector.tsx
@@ -1,9 +1,20 @@
-import { Button, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
+import { LucideProps } from "lucide-react";
+import { Glasses, Smile } from "lucide-react";
 
 interface UserTypeSelectorProps {
   value: string;
   onChange: (value: string) => void;
 }
+
+const IconInWraparound = ({
+  children,
+  value,
+}: LucideProps & { value: string }) => (
+  <Box sx={{ borderRadius: "8px", padding: "4px", border: "1px dotted" }}>
+    {children}
+  </Box>
+);
 
 export default function UserTypeSelector({
   value,
@@ -12,13 +23,16 @@ export default function UserTypeSelector({
   return (
     <Stack mb={2} spacing={2} direction="row" aria-label="Тип користувача">
       <Button
+        startIcon={<IconInWraparound value={value} children={<Smile />} />}
         fullWidth
+        // sx={{height: "48px"}}
         variant={value === "patient" ? "contained" : "outlined"}
         onClick={() => onChange("patient")}
       >
         {"Пацієнт"}
       </Button>
       <Button
+        startIcon={<IconInWraparound value={value} children={<Glasses />} />}
         fullWidth
         variant={value === "doctor" ? "contained" : "outlined"}
         onClick={() => onChange("doctor")}
