4a8a0b0ef8d60ffbb7ac618d54a95c81bc1c14f5
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-04-06 14:34:50 +0300

    PatientAccountPersonalInfo hardcoded data

--- src/pages/PatientAccountPersonalInfo.tsx
+++ src/pages/PatientAccountPersonalInfo.tsx
@@ -1,7 +1,19 @@
-import { Box, Button, Skeleton, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Skeleton,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 import { CustomizedPaper } from "~atomic/index";
-import { EditIcon } from "lucide-react";
+import { AlertTriangleIcon, EditIcon } from "lucide-react";
+
+import avatar from "~/assets/images/avatar.png";
 
 const EditButton = () => (
   <Button startIcon={<EditIcon style={{ flexShrink: 0 }} size={22} />}>
@@ -13,19 +25,70 @@ export function PatientAccountPersonalInfo() {
   return (
     <>
       <CustomizedPaper>
-        <Stack direction="row" justifyContent="space-between">
+        <Stack direction="row" justifyContent="space-between" mb="24px">
           <Typography variant="h5">
             {"Контактна інформація та авторизація"}
           </Typography>
           <EditButton />
         </Stack>
 
-        <Box sx={{ mt: "24px", display: "flex", gap: 2 }}>
-          <Skeleton variant="rounded" sx={{ height: 100, width: 100 }} />
+        {/* <Stack direction="row" gap={2}>
+          <Skeleton variant="rounded" sx={{ height: 168, width: 168 }} />
           <Skeleton variant="rounded" sx={{ height: 150, width: "100%" }} />
-        </Box>
+        </Stack> */}
+
+        <Stack direction="row">
+          <img src={avatar} alt="avatar" />
+
+          <TableContainer
+            sx={{
+              width: "auto",
+              "& td": { borderBottom: "none" },
+            }}
+          >
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Stack direction="row" gap={2}>
+                      <AlertTriangleIcon color="red" />
+                      <Typography variant="caption" color="red">
+                        Медкарта не підключена до eHealth
+                      </Typography>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Typography variant="body2">Номер телефону</Typography>
+                  </TableCell>
+
+                  <TableCell>
+                    <Typography variant="body2">+38 (093) 23 324 23</Typography>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography variant="body2">Електронна пошта</Typography>
+                  </TableCell>
+
+                  <TableCell>
+                    <Typography variant="body2">taras_shv@gmail.com</Typography>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Stack>
       </CustomizedPaper>
 
+      {/* ///       Персональні дані          /// */}
       <CustomizedPaper>
         <Stack direction="row" justifyContent="space-between">
           <Typography variant="h5">{"Персональні дані"}</Typography>
@@ -33,16 +96,76 @@ export function PatientAccountPersonalInfo() {
           <EditButton />
         </Stack>
 
-        <Skeleton variant="text" sx={{ height: 150 }} />
+        {/* <Skeleton variant="text" sx={{ height: 150 }} /> */}
 
-        <Stack direction="row" justifyContent="space-between">
+        <TableContainer
+          sx={{
+            display: "inline-block",
+            width: "auto",
+            "& td": { borderBottom: "none" },
+          }}
+        >
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body2">ПІБ</Typography>
+                </TableCell>
+
+                <TableCell>
+                  <Typography variant="body2">Шевченко Тарас</Typography>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body2">Дата народження</Typography>
+                </TableCell>
+
+                <TableCell>
+                  <Typography variant="body2">09.03.1814</Typography>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body2">ІПН</Typography>
+                </TableCell>
+
+                <TableCell>
+                  <Typography variant="body2">не вказано</Typography>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body2">Стать</Typography>
+                </TableCell>
+
+                <TableCell>
+                  <Typography variant="body2">не вказано</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          mt="24px"
+          mb="24px"
+        >
           <Typography variant="h5">
             {"Документи, що засвідчують особу"}
           </Typography>
           <EditButton />
         </Stack>
 
-        <Skeleton variant="text" />
+        <Typography variant="body2">
+          Документи, що засвідчують особу не додані.
+        </Typography>
+        {/* <Skeleton variant="text" /> */}
       </CustomizedPaper>
     </>
   );
