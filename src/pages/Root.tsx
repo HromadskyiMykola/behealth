07b233142d01f5d6fd3e5e819790305b424520b8
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-04-10 02:01:53 +0300

    fix container in patient account pages

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,24 +1,13 @@
-import { Outlet, useLocation } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 import Footer from "~/components/Footer/Footer";
 import Header from "~/components/Header/Header";
-import { ERouteNames } from "~/routes/routeNames";
-import { Container } from "@mui/material";
 
 export function Root() {
-  const { pathname } = useLocation();
-  const firstPath = pathname.split("/")[1];
 
   return (
     <>
       <Header />
-      {firstPath === ERouteNames.PATIENT_ACCOUNT ? (
-        <Container sx={{mb: '30px', mt: '30px'}}>
-          <Outlet />
-        </Container>
-      ) : (
-        <Outlet />
-      )}
+      <Outlet />
       <Footer />
     </>
   );
