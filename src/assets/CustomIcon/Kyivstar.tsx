044b62946c4a530c0977a08bbc23d0444b10a415
Aptechka <samsungt285v@gmail.com>
2023-04-15 16:41:23 +0300

    Refactoring code

--- src/assets/CustomIcon/Kyivstar.tsx
+++ src/assets/CustomIcon/Kyivstar.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import SvgIcon from "@mui/material/SvgIcon";
 import { SvgIconsProps } from "../../common/types-and-interfaces";
 
 const Kyivstar = (props: SvgIconsProps) => {
-  console.log(props);
   return (
     <svg {...props}>
       <path
---
+++ src/components/Addresses-and-info-doctor/index.ts
@@ -0,0 +1,1 @@
+export * from "./Addresses-and-info-doctor";
