d74125566172b248d5d2fcca587ae8a8ba2d6a80
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-04-28 20:34:58 +0300

    add NavTabs mobile vers; some fixs; (#104)

--- src/components/patient-Account-Personal-Info/index.ts
+++ src/components/patient-Account-Personal-Info/index.ts
@@ -5,3 +5,4 @@ export * from "./Personal-Data-Edit";
 export * from "./Identity-Documents";
 export * from "./Identity-DocumentsEdit";
 export * from "./Nav-Tabs";
+export * from "./Nav-Tabs-With-Drawer";
--- src/pages/Patient-Account-Page.tsx
+++ src/pages/Patient-Account-Page.tsx
@@ -1,15 +1,27 @@
 import { Helmet } from "react-helmet";
 import { Outlet } from "react-router-dom";
 
-import { Grid, Typography, Container } from "@mui/material";
+import {
+  Paper,
+  Stack,
+  Typography,
+  Container,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
 import { BreadcrumbsUkr } from "~/components/atomic/index";
 
-import { NavTabs } from "~/components/patient-Account-Personal-Info";
+import {
+  NavTabs,
+  NavTabsWithDrawer,
+} from "~/components/patient-Account-Personal-Info";
 import { usePatientFetchingData } from "~/common";
 
 export const PatientAccountPage = () => {
   const { patientPersonalData } = usePatientFetchingData();
+  const { breakpoints } = useTheme();
+  const isDownSm = useMediaQuery(breakpoints.down("md"));
 
   return (
     <>
@@ -22,27 +34,32 @@ export const PatientAccountPage = () => {
       </Helmet>
 
       <Container sx={{ mb: "30px", mt: "30px" }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <BreadcrumbsUkr />
-          </Grid>
+        <Stack spacing={3}>
+          {isDownSm && <NavTabsWithDrawer />}
 
-          <Grid item xs={12}>
-            <Typography variant="h4">
-              Вітаємо
-              {patientPersonalData?.firstName &&
-                `, ${patientPersonalData.firstName}`}
-            </Typography>
-          </Grid>
+          <BreadcrumbsUkr />
 
-          <Grid item xs={3}>
-            <NavTabs />
-          </Grid>
+          <Typography
+            variant="h4"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Вітаємо
+            {patientPersonalData?.firstName &&
+              `, ${patientPersonalData.firstName}`}
+          </Typography>
 
-          <Grid item xs>
-            <Outlet />
-          </Grid>
-        </Grid>
+          <Stack spacing={3} direction="row" alignItems="flex-start">
+            {!isDownSm && (
+              <Paper sx={{ borderRadius: "12px" }}>
+                <NavTabs />
+              </Paper>
+            )}
+
+            <Stack spacing={3} width="100%">
+              <Outlet />
+            </Stack>
+          </Stack>
+        </Stack>
       </Container>
     </>
   );
