996be0e2fc5995387c76ff7026e4de65793b88b7
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-04-30 16:05:33 +0300

    add mock-client

---
+++ src/common/api-mock-client.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { coloredLog } from "./api-Helpers";
+
+import doctorsData from "../mock-data/docs-mock-data.json";
+import clinicsData from "../mock-data/clinics-mock-data.json";
+
+const mockClient = axios.create();
+
+const mock = new MockAdapter(mockClient);
+
+const queryData = [
+  "city",
+  "district",
+  [
+    {
+      datatype: "",
+      name: "",
+      id: "",
+    },
+  ],
+];
+
+mock.onGet("/doctors").reply(200, doctorsData);
+mock.onGet("/clinics").reply(200, clinicsData);
+mock.onGet("/search").reply((config) => {
+  console.log("req orig PARAMS >>>", config.params);
+  const { city, district, query } = config.params;
+  if (city !== queryData[0] && district !== queryData[1]) {
+  }
+  // TODO: query processing
+  coloredLog("response >>>", config.params);
+  return [
+    200,
+    {
+      // // TODO: return value
+    },
+  ];
+});
+
+export { mockClient };
