33fc4cdb9477100869ab45ce02984ca96b6d0f54
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-05-01 18:04:14 +0300

    Doctors page2 (#117)
    * import mock-data to doctors-page
    
    * mod mock-api
    
    * search on doctors page has been implemented

--- src/components/doctorsPage/SearchBar.tsx
+++ src/components/doctorsPage/SearchBar.tsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { SyntheticEvent, useEffect, useMemo, useState } from "react";
 
 import {
+  Autocomplete,
+  AutocompleteChangeDetails,
+  AutocompleteChangeReason,
   Button,
+  IconButton,
   InputAdornment,
   MenuItem,
   SelectChangeEvent,
@@ -15,16 +19,71 @@ import {
   CustomizedPaper,
   SelectWithPlaceholder,
 } from "../atomic";
-import { useDeviceType } from "~/common";
+import { TDoctor, useDeviceType } from "~/common";
 
-export const SearchBar = () => {
-  const [selectedValue, setSelectedValue] = useState("");
+interface ISearchBar {
+  doctors: TDoctor[];
+  setFilteredDoctors: (value: React.SetStateAction<TDoctor[]>) => void;
+}
+
+const specialties = (doctors: TDoctor[]) =>
+  doctors.reduce((acc: string[], doc) => {
+    if (!acc.includes(doc.specialty)) {
+      acc.push(doc.specialty);
+    }
+    return acc;
+  }, []);
+
+const namesList = (selectedSpec: string, doctors: TDoctor[]) =>
+  doctors
+    .filter((doc) => selectedSpec === "" || doc.specialty === selectedSpec)
+    .map((doc) => doc.name);
+
+export const SearchBar = ({ doctors, setFilteredDoctors }: ISearchBar) => {
+  const [selectedSpec, setSelectedSpec] = useState("");
+  const [searchString, setSearchString] = useState("");
   const { custom } = useTheme().palette;
   const { isSmDown } = useDeviceType();
 
-  const handleSelectChange = (event: SelectChangeEvent<unknown>) => {
-    setSelectedValue(event.target.value as string);
+  // const [value, setValue] = useState(null);
+
+  const filteredDoctors = useMemo(
+    () => namesList(selectedSpec, doctors),
+    [selectedSpec, doctors]
+  );
+
+  const handleSelectChange = (e: SelectChangeEvent<any>) => {
+    setSelectedSpec(e.target.value as string);
   };
+
+  const onSubmitSearch = () => {
+    const filteredList = doctors.filter(
+      (doctor) =>
+        doctor.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1
+    );
+    console.log("!!!", searchString, "list >>", filteredList);
+
+    setFilteredDoctors(filteredList);
+  };
+
+  const onChangeSearch = (
+    event: SyntheticEvent<Element, Event>,
+    value: string | null,
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<string> | undefined
+  ) => {
+    setSearchString(value || "");
+
+    // if (reason === "selectOption" || reason === "createOption") {
+    //   onSubmitSearch();
+    // }
+
+    console.log(event.target, "val>>", value, "reas>>", reason, "det", details);
+  };
+
+  useEffect(() => {
+    searchString && onSubmitSearch();
+  }, [searchString]);
 
   return (
     <CustomizedPaper
@@ -40,25 +99,42 @@ export const SearchBar = () => {
           sx={{ display: "flex", flexBasis: "40%", minWidth: "150px" }}
           label="Спеціальність"
           placeholder="Обрати"
-          value={selectedValue}
+          value={selectedSpec}
           onChange={handleSelectChange}
         >
-          <MenuItem value={1}>Option 1</MenuItem>
-          <MenuItem value={2}>Option 2</MenuItem>
-          <MenuItem value={3}>Option 3</MenuItem>
+          {specialties(doctors).map((spec) => (
+            <MenuItem key={spec} value={spec}>
+              {spec}
+            </MenuItem>
+          ))}
         </SelectWithPlaceholder>
 
-        <CustomizedInput
-          sx={{ display: "flex", flexBasis: "60%" }}
-          label="ПІБ лікаря"
-          placeholder="Введіть імʼя лікаря"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon color={custom.neutral70} />
-              </InputAdornment>
-            ),
-          }}
+        <Autocomplete
+          freeSolo
+          loading
+          loadingText="Завантаження..."
+          fullWidth
+          options={filteredDoctors}
+          onChange={onChangeSearch}
+          renderInput={(params) => (
+            <CustomizedInput
+              {...params}
+              label="ПІБ лікаря"
+              placeholder="Введіть імʼя лікаря"
+              InputProps={{
+                ref: params.InputProps.ref,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton>
+                      <SearchIcon color={custom.neutral70} />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                endAdornment: params.InputProps.endAdornment,
+              }}
+              // onChange={onChangeSearch}
+            />
+          )}
         />
 
         <Button sx={{ minWidth: "175px" }} variant="contained">
